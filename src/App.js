c3505616fc0378d9d48ff3aaf1e2a0a58144718e
Facundo Rodriguez <facundo.rodriguez@fluxit.com.ar>
2020-01-21 15:49:06 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import imagen from "./cryptomonedas.png";
 import Formulario from "./components/Formulario";
-import CriptoService from './services/CriptoService'
+import CriptoService from "./services/CriptoService";
 import Quotation from "./components/Quotation";
 
 function App() {
+  const [moneyType, saveMoneyType] = useState("");
+  const [coinType, saveCoinType] = useState("");
+  const [coinInfo, saveCoinInfo] = useState("");
 
-  const [moneyType, saveMoneyType] = useState('');
-  const [coinType, saveCoinType] = useState('');
-  const [coinInfo, saveCoinInfo] = useState('')
-
-  useEffect(()=>{
-
-    if(coinType === '' || moneyType === ''){
+  useEffect(() => {
+    if (coinType === "" || moneyType === "") {
       return;
     }
 
-    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${coinType}&tsyms=${moneyType}`
-    const critpoService =  new CriptoService();
-    const coinDetail =  critpoService.getCoinPrice(url);
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${coinType}&tsyms=${moneyType}`;
+    const critpoService = new CriptoService();
+    const coinDetail = critpoService.getCoinPrice(url);
 
-    coinDetail.then((value) => {
-      saveCoinInfo(value.data.DISPLAY[coinType][moneyType])
-    }, (reason) => {
-      console.error(reason);
-    });
- 
-  },[moneyType,coinType])
+    const coinsDetail = async () => {
+      const value = await coinDetail;
+      saveCoinInfo(value.data.DISPLAY[coinType][moneyType]);
+    };
+
+    coinsDetail();
+  }, [moneyType, coinType]);
 
   return (
     <div className="container">
@@ -37,9 +35,12 @@ function App() {
         </div>
         <div className="one-half column">
           <h1>Cotiza Criptomonedas al Instante</h1>
-          <Formulario saveMoneyType={saveMoneyType} saveCoinType={saveCoinType} />
+          <Formulario
+            saveMoneyType={saveMoneyType}
+            saveCoinType={saveCoinType}
+          />
 
-          <Quotation coinInfo={coinInfo}/>
+          <Quotation coinInfo={coinInfo} />
         </div>
       </div>
     </div>
